578b606d25f0eab4b7f296f2d426943d7bde665b
furka <thomas-vranckx@gmail.com>
2026-06-26 08:48:10 +0100

    fatal bugfix

--- src/utils/bypass-title.js
+++ src/utils/bypass-title.js
@@ -5,3 +5,5 @@ const bypassModifierKey = (() => {
 })();
 
 export const bypassTitle = `Add or attach units and enhancements without restrictions.\n\nOr hold ${bypassModifierKey} while clicking or dragging.`;
+
+export const enforceTitle = `Enforce army-building restrictions when adding or attaching units and enhancements.\n\nHold ${bypassModifierKey} while clicking or dragging to bypass.`;
